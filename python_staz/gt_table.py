456e8a6db5bc3c095faae550f2e33d030189cc0f
Patricie Skaláková <158295213+Patricie34@users.noreply.github.com>
2024-03-12 07:10:09 +0100

    Update gt_table.py

--- python_staz/gt_table.py
+++ python_staz/gt_table.py
@@ -4,7 +4,8 @@ import pandas as pd
 # Načtení dat z VCF souboru
 callset = allel.read_vcf("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz")
 
-# Extrahování informací o genotypu vzorků
+# Extrahování informací o genotypu
+# GenotypeArray - dat. struktura z knihovny scikit-allel, umožňuje ukládat a manipulovat z genotypy z .vcf souboru 
 genotypes = allel.GenotypeArray(callset['calldata/GT'])
 
 # Vytvoření tabulky pro genotypy
@@ -14,4 +15,4 @@ gt_table.index.name = 'Variant'
 # Připojení tabulky genotypů k původní tabulce
 table_with_gt = pd.concat([table, gt_table], axis=1)
 
-print(table_with_genotypes.head())
+print(table_with_gt.head())
