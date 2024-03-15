10b0a36bc3eae67a4e6a532c0cc8e4a181335077
user <user@patricie-0.patricie.medgen-ns.svc.cluster.local>
2024-03-15 15:01:36 +0100

    correct script

--- python_staz/pd_table.py
+++ python_staz/pd_table.py
@@ -1,7 +1,26 @@
 import allel
 import pandas as pd
 
-callset = allel.read_vcf("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz")
+# specify directory where vcf occur -> specify only if using linux container
+dir="/home/user/project/results/test/vcf_test/"
+
+# load vcf but missing GT and ALT
+callset = allel.vcf_to_dataframe(dir + "TEST_HaplotypeCaller_HG002_HiSeq_2MergedRuns.vcf", fields=['CHROM','POS', 'ID', 'REF', 'ALT', 'GT','QUAL'])
+
+
+#df2 = pd.DataFrame(callset)
+callset.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
+
+# rearange GT field
+gt_vcf = allel.read_vcf(dir + "TEST_HaplotypeCaller_HG002_HiSeq_2MergedRuns.vcf", fields = 'GT')
+
+final_tab = pd.concat([callset, pd.DataFrame([str(x.flatten().tolist()).replace(', ', '/').replace('[', '').replace(']', '') for x in gt_vcf['calldata/GT'][:]])], axis=1)
+final_tab.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'GT']
+
+# add column 'VAR_ID'
+>>> test['CHROM'][0] + ":" + str(test['POS'][0]) + "_" + test['ALT_1'][0]
+# 'chr1:10492_T'
+
 # print(callset.keys())
 # dict_keys = (['samples', 'calldata/GT', 'variants/ALT', 'variants/CHROM', 'variants/FILTER_PASS', 'variants/ID', 'variants/POS', 'variants/QUAL', 
 # 'variants/REF'])
@@ -9,8 +28,8 @@ callset = allel.read_vcf("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz")
 # table = allel.vcf_to_dataframe("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz")
 
 # Tabulka variant
-table = allel.vcf_to_dataframe("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz", fields=['CHROM','POS', 'ID', 'REF', 'ALT_1', 'ALT_2', 'ALT_3', 'QUAL','FILTER_PASS'])
-table.set_index('CHROM', inplace=True)
+#table = allel.vcf_to_dataframe("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz", fields=['CHROM','POS', 'ID', 'REF', 'ALT_1', 'ALT_2', 'ALT_3', 'QUAL','FILTER_PASS'])
+#table.set_index('CHROM', inplace=True)
 
 print(table.head())
 #print(table.columns)
