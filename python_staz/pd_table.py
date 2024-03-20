7b838936f2299b9bfd738a73b14589521ec44e2b
user <user@patricie-0.patricie.medgen-ns.svc.cluster.local>
2024-03-20 08:29:24 +0100

    new version

--- python_staz/pd_table.py
+++ python_staz/pd_table.py
@@ -1,35 +1,68 @@
 import allel
 import pandas as pd
+import argparse
 
-# specify directory where vcf occur -> specify only if using linux container
-dir="/home/user/project/results/test/vcf_test/"
+# parse parametres
+parser = argparse.ArgumentParser(description='Tool to benchmark MGI and ilumina vcfs to GIB true dataset')
+
+parser.add_argument('--true', help='true HG002 variant call file (vcf) downaloaded from GIB') 
+parser.add_argument('--mgi', help='MGI vcf')
+parser.add_argument('--ilumina', help='ilumina vcf')
+
+args = parser.parse_args()
+true=args.true
+mgi=args.mgi
+ilumina=args.ilumina
 
 # load vcf but missing GT and ALT
-callset = allel.vcf_to_dataframe(dir + "TEST_HaplotypeCaller_HG002_HiSeq_2MergedRuns.vcf", fields=['CHROM','POS', 'ID', 'REF', 'ALT', 'GT','QUAL'])
-
+print("load vcfs")
+callset_true = allel.vcf_to_dataframe(true, fields=['CHROM','POS', 'ID', 'REF', 'ALT', 'GT','QUAL'])
+callset_1 = allel.vcf_to_dataframe(mgi, fields=['CHROM','POS', 'ID', 'REF', 'ALT', 'GT','QUAL'])
+callset_2 = allel.vcf_to_dataframe(ilumina, fields=['CHROM','POS', 'ID', 'REF', 'ALT', 'GT','QUAL'])
+print("load vcfs done")
 
 #df2 = pd.DataFrame(callset)
-callset.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
+callset_true.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
+callset_1.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
+callset_2.drop(['ALT_2', 'ALT_3'], axis=1, inplace=True)
 
 # rearange GT field
-gt_vcf = allel.read_vcf(dir + "TEST_HaplotypeCaller_HG002_HiSeq_2MergedRuns.vcf", fields = 'GT')
+print("load GT")
+gt_vcf_true = allel.read_vcf(true, fields = 'GT')
+gt_vcf_1 = allel.read_vcf(mgi, fields = 'GT')
+gt_vcf_2 = allel.read_vcf(ilumina, fields = 'GT')
+print("load GT done")
 
-final_tab = pd.concat([callset, pd.DataFrame([str(x.flatten().tolist()).replace(', ', '/').replace('[', '').replace(']', '') for x in gt_vcf['calldata/GT'][:]])], axis=1)
-final_tab.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'GT']
+callset_true.reset_index(drop=True, inplace=True)
+callset_1.reset_index(drop=True, inplace=True)
+callset_2.reset_index(drop=True, inplace=True)
 
-# add column 'VAR_ID'
->>> test['CHROM'][0] + ":" + str(test['POS'][0]) + "_" + test['ALT_1'][0]
-# 'chr1:10492_T'
+final_tab_true = pd.concat([callset_true, pd.DataFrame([str(x.flatten().tolist()).replace(', ', '/').replace('[', '').replace(']', '') for x in gt_vcf_true['calldata/GT'][:]])], axis=1)
+final_tab_true.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'GT']
 
-# print(callset.keys())
-# dict_keys = (['samples', 'calldata/GT', 'variants/ALT', 'variants/CHROM', 'variants/FILTER_PASS', 'variants/ID', 'variants/POS', 'variants/QUAL', 
-# 'variants/REF'])
+final_tab_1 = pd.concat([callset_1, pd.DataFrame([str(x.flatten().tolist()).replace(', ', '/').replace('[', '').replace(']', '') for x in gt_vcf_1['calldata/GT'][:]])], axis=1)
+final_tab_1.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'GT']
 
-# table = allel.vcf_to_dataframe("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz")
+final_tab_2 = pd.concat([callset_2, pd.DataFrame([str(x.flatten().tolist()).replace(', ', '/').replace('[', '').replace(']', '') for x in gt_vcf_2['calldata/GT'][:]])], axis=1)
+final_tab_2.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'GT']
 
-# Tabulka variant
-#table = allel.vcf_to_dataframe("HG002_HiSeq_sorted_mapped_haplCall.g.vcf.gz", fields=['CHROM','POS', 'ID', 'REF', 'ALT_1', 'ALT_2', 'ALT_3', 'QUAL','FILTER_PASS'])
-#table.set_index('CHROM', inplace=True)
 
-print(table.head())
-#print(table.columns)
+# create and absolute variant identificator columns for all tables (vcfs)
+final_tab_true['ident'] = [str(final_tab_true['CHROM'][i] + ":" + str(final_tab_true['POS'][i]) + "_" + final_tab_true['ALT'][i]) for i in range(0, len(final_tab_true['CHROM']))]
+final_tab_1['ident'] = [str(final_tab_1['CHROM'][i] + ":" + str(final_tab_1['POS'][i]) + "_" + final_tab_1['ALT'][i]) for i in range(0, len(final_tab_1['CHROM']))]
+final_tab_2['ident'] = [str(final_tab_2['CHROM'][i] + ":" + str(final_tab_2['POS'][i]) + "_" + final_tab_2['ALT'][i]) for i in range(0, len(final_tab_2['CHROM']))]
+
+# identification of True/False positive and negative results
+
+## final_table as True dataset
+true_pos_1 = sum(final_tab_true['ident'].isin(final_tab_1['ident']))/len(final_tab_true['ident']) * 100
+false_pos_1= sum(~final_tab_1['ident'].isin(final_tab_true['ident']))/len(final_tab_1['ident']) * 100
+false_neg_1 = sum(~final_tab_true['ident'].isin(final_tab_1['ident']))/len(final_tab_1['ident']) * 100
+
+true_pos_2 = sum(final_tab_true['ident'].isin(final_tab_2['ident']))/len(final_tab_true['ident']) * 100
+false_pos_2 = sum(~final_tab_2['ident'].isin(final_tab_true['ident']))/len(final_tab_2['ident']) * 100
+false_neg_2 = sum(~final_tab['ident'].isin(final_tab2['ident']))/len(final_tab2['ident']) * 100
+
+print(true_pos_1)
+print(false_pos_1)
+print(false_neg_1)
